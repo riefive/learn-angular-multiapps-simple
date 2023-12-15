01474d2fa8bcacd6f35076179189825cc42a081c
riefive <muhammad.rifai@salt.co.id>
2023-12-15 11:15:46 +0700

    fix(main-route): solve import module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { App1SharedModule } from '../../projects/application1/src/app/app.module';
+import { App2SharedModule } from '../../projects/application2/src/app/app.module';
 
-const routes: Routes = [
-  { path: 'app1', loadChildren: () => import('../../projects/application1/src/app/app.module').then(m => m.App1SharedModule) },
-  { path: 'app2', loadChildren: () => import('../../projects/application2/src/app/app.module').then(m => m.App2SharedModule) },
-  { path: '**', redirectTo: '/app1/one'}
-];
+const isImport = false;
+let routes: Routes = [];
+
+if (isImport) {
+  routes = [
+    { path: 'app1', loadChildren: () => import('../../projects/application1/src/app/app.module').then(m => m.App1SharedModule) },
+    { path: 'app2', loadChildren: () => import('../../projects/application2/src/app/app.module').then(m => m.App2SharedModule) },
+    { path: '**', redirectTo: '/app1/one'}
+  ];
+} else {
+  routes = [
+    { path: 'app1', loadChildren: '../../projects/application1/src/app/app.module#App1SharedModule' },
+    { path: 'app2', loadChildren: '../../projects/application2/src/app/app.module#App1SharedModule' },
+    { path: '**', redirectTo: '/app1/one'}
+  ];
+}
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes), App1SharedModule.forRoot(), App2SharedModule.forRoot()],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
