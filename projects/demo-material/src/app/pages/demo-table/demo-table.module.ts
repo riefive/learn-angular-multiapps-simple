a3f2195bac6d1a53dce14b2069425c6211126c76
riefive <muhammad.rifai@salt.co.id>
2023-12-18 15:41:58 +0700

    fix(material): add demo table module with export key

--- projects/demo-material/src/app/pages/demo-table/demo-table.module.ts
+++ projects/demo-material/src/app/pages/demo-table/demo-table.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { DemoTableComponent } from './demo-table.component';
 import { DemoTableRoutingModule } from './demo-table-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [DemoTableComponent],
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
     DemoTableRoutingModule
-  ]
+  ],
+  exports: [DemoTableComponent]
 })
 export class DemoTableModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { App1SharedModule } from 'projects/application1/src/app/app.module';
 import { App2SharedModule } from 'projects/application2/src/app/app.module';
-import { AppModule as AppMaterialSharedModule } from 'projects/demo-material/src/app/app.module';
+// import { AppModule as AppMaterialSharedModule } from 'projects/demo-material/src/app/app.module';
 import { LibBeehiveUiSharedModule } from 'projects/lib-beehive-ui-shared/src/public-api';
 import { NavComponent } from './nav/nav.component';
 import { FirstComponent } from './first/first.component';
@@ -22,7 +22,6 @@ import { FirstComponent } from './first/first.component';
     AppRoutingModule,
     App1SharedModule.forRoot(),
     App2SharedModule.forRoot(),
-    AppMaterialSharedModule,
     LibBeehiveUiSharedModule,
   ],
   providers: [],
