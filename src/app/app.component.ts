84fd7d665bbf6ac97b08d6f402f24b0593ef20de
riefive <muhammad.rifai@salt.co.id>
2023-12-21 13:47:00 +0700

    feat(auth): update change nav component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IMenu } from 'projects/lib-beehive-ui-shared/src/public-api';
 import { AuthService } from './services/auth.service';
 
+const menuRaws: IMenu[] = [
+  { path: '/pages/beehive-red-happy', name: 'Red-Happy' } as IMenu,
+  { path: '/pages/beehive-red-angry', name: 'Red-Angry' } as IMenu,
+  { path: '/pages/beehive-green-happy', name: 'Green-Happy' } as IMenu,
+  { path: '/pages/beehive-green-angry', name: 'Green-Angry' } as IMenu,
+  { path: '/pages/beehive-blue-happy', name: 'Blue-Happy' } as IMenu,
+  { path: '/pages/beehive-blue-angry', name: 'Blue-Angry' } as IMenu,
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,17 +25,11 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.isLogin = this.authSrv.IsLogin;
+    this.menus = this.isLogin ? menuRaws : [];
     this.authSrv.loginChange.subscribe((value) => {
+      this.menus = value ? menuRaws : [];
       this.isLogin = value;
     });
-    this.menus = [
-      { path: '/pages/beehive-red-happy', name: 'Red-Happy' } as IMenu,
-      { path: '/pages/beehive-red-angry', name: 'Red-Angry' } as IMenu,
-      { path: '/pages/beehive-green-happy', name: 'Green-Happy' } as IMenu,
-      { path: '/pages/beehive-green-angry', name: 'Green-Angry' } as IMenu,
-      { path: '/pages/beehive-blue-happy', name: 'Blue-Happy' } as IMenu,
-      { path: '/pages/beehive-blue-angry', name: 'Blue-Angry' } as IMenu,
-    ];
   }
 
   ngOnDestroy() {
