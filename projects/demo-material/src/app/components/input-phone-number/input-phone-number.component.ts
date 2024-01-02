f3613507f26cbdd85a5dce8a1842a110b207ddef
riefive <muhammad.rifai@salt.co.id>
2024-01-02 16:23:31 +0700

    fix(accessor-two): add handle input only number

--- projects/demo-material/src/app/components/input-phone-number/input-phone-number.component.ts
+++ projects/demo-material/src/app/components/input-phone-number/input-phone-number.component.ts
@@ -88,4 +88,12 @@ export class InputPhoneNumberComponent implements ControlValueAccessor {
     this.hasChanged = true;
     this.onTouched(this.value);
   }
+
+  HandleNumberOnly(event: any): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
 }
