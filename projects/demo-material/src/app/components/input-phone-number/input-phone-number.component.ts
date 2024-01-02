646f52fc14dd57a0b9cf0bf29418fcdb34b69460
riefive <muhammad.rifai@salt.co.id>
2024-01-02 14:43:42 +0700

    feat(accessor-two): introduced input phone number

---
+++ projects/demo-material/src/app/components/input-phone-number/input-phone-number.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-phone-number',
+  templateUrl: './input-phone-number.component.html',
+  styleUrls: ['./input-phone-number.component.scss']
+})
+export class InputPhoneNumberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
