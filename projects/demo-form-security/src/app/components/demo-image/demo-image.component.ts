9566c88ab3a722cba064399bb86a0f80cebe1eb7
riefive <muhammad.rifai@salt.co.id>
2024-01-03 10:44:42 +0700

    feat(location): implement location feature

--- projects/demo-form-security/src/app/components/demo-image/demo-image.component.ts
+++ projects/demo-form-security/src/app/components/demo-image/demo-image.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 @Component({
   selector: 'app-demo-image',
   templateUrl: './demo-image.component.html',
-  styleUrls: ['./demo-image.component.css']
+  styleUrls: ['./demo-image.component.css'],
+  providers: [
+    Location, 
+    { provide: LocationStrategy, useClass: PathLocationStrategy }
+  ]
 })
 export class DemoImageComponent implements OnInit {
   sourceImage: string = '';
+  currentPath: string = '';
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
     this.sourceImage = 'https://placehold.co/600x400';
+    this.currentPath = this.location.path();
+  }
+
+  HandleBack() {
+    this.location.back();
+  }
+  
+  HandleForward() {
+    this.location.forward();
   }
 
 }
